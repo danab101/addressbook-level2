013dc0b9ce9eee8afe21feba91b15f594499c4c6
Dana Baghabrah <d.baghabra@gmail.com>
2017-02-10 13:08:01 +0800

    Use ISP Printable;

---
+++ src/seedu/addressbook/data/person/Printable.java
@@ -0,0 +1,9 @@
+package seedu.addressbook.data.person;
+
+/**
+ * An interface in the addressbook which creates a printable string representation of Name, Phone, Email, and Address.
+ */
+
+public interface Printable {
+	public String getPrintableString();
+}
